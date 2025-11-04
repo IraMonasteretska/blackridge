e1550e568c99b498860ccf82398b9f5d5a76264b
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2025-11-04 16:19:36 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(window).on('scroll load', function () {
+        if ($(this).scrollTop() > 10) {
+            $('.header').addClass('scroll');
+        } else {
+            $('.header').removeClass('scroll');
+        }
+    });
+});
