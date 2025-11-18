97874bee403064da412507021ed028d3edc48254
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2025-11-18 16:09:25 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -109,4 +109,41 @@ $(document).ready(function () {
         });
     });
 
+    // print
+    document.querySelector(".print-btn").addEventListener("click", function () {
+        window.print();
+    });
+
+
+
+    // slider - latest news
+    var swiper = new Swiper(".latestnews-slider", {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+        speed: 1200,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            576: {
+                slidesPerView: 1.5,
+                spaceBetween: 20,
+            },
+            767: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+           
+        },
+    });
+
+
+
+
 });
